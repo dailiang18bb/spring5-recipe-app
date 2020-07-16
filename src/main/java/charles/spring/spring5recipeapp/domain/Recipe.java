ff0c079293fb80a314ec0f06c523b5588066efc5
Charles <dailiang18bb@gmail.com>
2020-07-16 15:46:21 -0400

    Added Ingredient Entity and relationship. Closes #3 Recipe <==> Ingredient, OneToMany

--- src/main/java/charles/spring/spring5recipeapp/domain/Recipe.java
+++ src/main/java/charles/spring/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package charles.spring.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 
 @Entity
@@ -19,6 +20,9 @@ public class Recipe {
     private String directions;
     // TODO add
     // private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -89,6 +93,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
