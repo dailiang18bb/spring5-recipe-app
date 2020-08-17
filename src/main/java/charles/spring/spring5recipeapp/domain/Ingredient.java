e546dbda986c2d588517e58d5b7bd76e3503de6a
Charles <dailiang18bb@gmail.com>
2020-08-17 13:48:42 -0400

    Fixed Lombok Applied hashcode exclude

--- src/main/java/charles/spring/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/charles/spring/spring5recipeapp/domain/Ingredient.java
@@ -1,12 +1,14 @@
 package charles.spring.spring5recipeapp.domain;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
