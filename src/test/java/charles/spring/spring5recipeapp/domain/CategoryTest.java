c1027aa2cc8199a143c95b458b5e4b414680c501
Charles <dailiang18bb@gmail.com>
2020-08-27 14:13:21 -0400

    Added JUnit tests Tested Category Tested RecipeServiceImpl

---
+++ src/test/java/charles/spring/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package charles.spring.spring5recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setup(){
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 5L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
